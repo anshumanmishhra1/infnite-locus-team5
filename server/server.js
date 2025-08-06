233e08f56f4627c346ccf141738f08c1fdb14fa1
anshumanmishhra1 <anshumanmishhra1@gmail.com>
2025-08-06 13:08:51 +0530

    checkig login api

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-import connectDb from './config/db.js';
-import bcrypt from 'bcrypt';
-import User from './models/userSchema.js';
+import express from "express";
+import jwt from "jsonwebtoken";
+import connectDb from "./config/db.js";
+import bcrypt from "bcrypt";
+import User from "./models/userSchema.js";
 
 const app = express();
 connectDb();
 
 app.use(express.json());
 
-app.post('/user/signup', async (req, res) => {
+app.post("/user/signup", async (req, res) => {
   try {
     const user = req.body;
     const userPassword = user.password;
@@ -17,7 +17,7 @@ app.post('/user/signup', async (req, res) => {
     // Check if user with same email already exists
     const existingUser = await User.findOne({ email: user.email });
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: "User already exists" });
     }
 
     // Hash password
@@ -35,13 +35,24 @@ app.post('/user/signup', async (req, res) => {
 
     res.status(201).send("User created successfully");
   } catch (error) {
-    res.status(500).json({ message: 'Something went wrong', error: error.message });
+    res
+      .status(500)
+      .json({ message: "Something went wrong", error: error.message });
   }
 });
 
-app.post('/user/login', async (req,res)=>{
+app.post("/user/login", async (req, res) => {
+  try {
     const user = req.body;
-})
+    //checking if email exist or not
+    if (!(await User.findOne(user.email))) {
+      res.status(400).send("user doesn't exist check your mail");
+    }
+
+    const userPassword = user.password;
+    
+  } catch (error) {}
+});
 
 const port = 3000;
 app.listen(port, () => {
